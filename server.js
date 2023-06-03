974ebe5f4ad7346fd787ae5219f4ddb2fbeb0ba4
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-06-03 14:10:52 +0100

    connect to DB mongoose & handle unhandled promises

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 
-//Route files
-const bootcamps = require('./routes/bootcamps');
+const connectDB = require('./config/db');
 
 //Load env files
 dotenv.config({ path: './config/config.env' });
+
+//Connect to database
+connectDB();
+// connectDB().catch(console.log);
+
+//Route files
+const bootcamps = require('./routes/bootcamps');
 
 const app = express();
 
@@ -20,7 +26,14 @@ app.use('/api/v1/bootcamps', bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_env} mode on port ${PORT}`)
 );
+
+//Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error:${err.message}`);
+  //Close server &  exit process
+  server.close(() => process.exit(1));
+});
