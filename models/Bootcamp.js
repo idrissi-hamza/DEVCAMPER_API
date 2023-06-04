3f542c21d0cd86c82e83019e0b29ce037efb9090
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-06-04 12:04:38 +0100

    add geojson location geocoder middleware

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const geocoder = require('../utils/geocoder');
+
 const BootcampSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -103,6 +105,26 @@ const BootcampSchema = new mongoose.Schema({
 BootcampSchema.pre('save', function (next) {
   // comsole.log('Slugify ran'.this.name);
   this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+//Geocode & create location field
+BootcampSchema.pre('save', async function (next) {
+  const [loc] = await geocoder.geocode(this.address);
+  this.location = {
+    type: 'Point',
+    coordinates: [loc.longitude, loc.latitude],
+    formatedAdress: loc.formattedAddress,
+    street: loc.streetName,
+    city: loc.city,
+    state: loc.stateCode,
+    zipcode: loc.zipcode,
+    country: loc.countryCode,
+  };
+
+  //do not save address in DB we already set formatedAdress
+  this.address = undefined;
+
   next();
 });
 
