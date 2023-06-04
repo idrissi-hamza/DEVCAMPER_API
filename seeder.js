aa2fa59559a3b3b2011c4d4b52cd6785ccf94f5d
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-06-05 00:02:23 +0100

    update seeder to seed courses

--- seeder.js
+++ seeder.js
@@ -8,6 +8,7 @@ dotenv.config({ path: './config/config.env' });
 
 //load models
 const Bootcamp = require('./models/Bootcamp');
+const Course = require('./models/Course');
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI);
@@ -16,11 +17,15 @@ mongoose.connect(process.env.MONGO_URI);
 const bootcamps = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
 );
+const courses = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/courses.json`, 'utf-8')
+);
 
 //Import  into db
 const importData = async () => {
   try {
     await Bootcamp.create(bootcamps);
+    await Course.create(courses);
     console.log('data imported...'.green.inverse);
   } catch (err) {
     console.error(err);
@@ -31,6 +36,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Bootcamp.deleteMany();
+    await Course.deleteMany();
     console.log('data destroyed...'.red.inverse);
   } catch (err) {
     console.error(err);
