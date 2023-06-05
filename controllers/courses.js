e45707f00d06e1b0b40e19f81bcc8b13dc35afa6
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-06-05 17:33:59 +0100

    create Get single course request

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,5 +1,6 @@
 const asyncHandler = require('../middleware/async');
 const Course = require('../models/Course');
+const ErrorResponse = require('../utils/errorResponse');
 
 // @desc    Get all courses
 // @route   GET /api/v1/courses
@@ -23,5 +24,30 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
     success: true,
     count: courses.length,
     data: courses,
+  });
+});
+
+// @desc    Get  course
+// @route   GET /api/v1/courses/:id
+// @access  Public
+
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  
+
+  const course = await Course.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name description',
+  });
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  res.status(200).json({
+    success: true,
+    data: course,
   });
 });
