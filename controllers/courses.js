ab3315c5f54f0f6070700a79734d4de7c353be8c
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-06-05 21:57:53 +0100

    create Delete a single course method

--- controllers/courses.js
+++ controllers/courses.js
@@ -93,3 +93,20 @@ exports.updateCourse = asyncHandler(async (req, res, next) => {
 
   res.status(200).json({ success: true, data: course });
 });
+
+
+// @desc    Delete a course
+// @route   DELETE /api/v1/courses/:id
+// @access  Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+  if (!course) {
+    return next(
+      new ErrorResponse(`Course not found with id of ${req.params.id}`, 404)
+    );
+  }
+
+  await course.deleteOne()
+
+  res.status(200).json({ success: true, data: {} });
+});
