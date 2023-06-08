824d38df74f6fa2b2eb2f5a05c6e34fedc638336
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-06-08 12:00:16 +0100

    clean unecessary code

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,6 @@ const express = require('express');
 const { register, login, getMe } = require('../controllers/auth');
 
 const router = express.Router();
-const {protect}=require('../middleware/auth')
 router.route('/register').post(register);
 router.route('/login').post(login);
 router.route('/me').get(getMe);
