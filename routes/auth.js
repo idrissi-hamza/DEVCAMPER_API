4f12a48124e876b30d60e598025c0aa2eca5b2e7
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-06-08 11:21:44 +0100

    create a middleware to authorize routes on role

--- routes/auth.js
+++ routes/auth.js
@@ -2,9 +2,9 @@ const express = require('express');
 const { register, login, getMe } = require('../controllers/auth');
 
 const router = express.Router();
-const { protect } = require('../middleware/auth');
+const {protect}=require('../middleware/auth')
 router.route('/register').post(register);
 router.route('/login').post(login);
-router.route('/me').get(protect, getMe);
+router.route('/me').get(getMe);
 
 module.exports = router;
