eac2612c09851bc1813d097202ab1f445f21091b
A-IdrissiHamza <a.idrissihamza@gmail.com>
2023-06-09 15:49:39 +0100

    seed fake review data

--- seeder.js
+++ seeder.js
@@ -10,6 +10,7 @@ dotenv.config({ path: './config/config.env' });
 const Bootcamp = require('./models/Bootcamp');
 const Course = require('./models/Course');
 const User = require('./models/User');
+const Review = require('./models/Review');
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI);
@@ -24,6 +25,9 @@ const courses = JSON.parse(
 const users = JSON.parse(
   fs.readFileSync(`${__dirname}/_data/users.json`, 'utf-8')
 );
+const reviews = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/reviews.json`, 'utf-8')
+);
 
 //Import  into db
 const importData = async () => {
@@ -31,6 +35,7 @@ const importData = async () => {
     await Bootcamp.create(bootcamps);
     await Course.create(courses);
     await User.create(users);
+    await Review.create(reviews);
     console.log('data imported...'.green.inverse);
   } catch (err) {
     console.error(err);
